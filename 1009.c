c0d7e9a15cd9d576b89f7895389f9dc4aeb2f8d5
Kh-Atia-Rahaman <238537083+Kh-Atia-Rahaman@users.noreply.github.com>
2025-11-05 12:00:00 +0600

    Beecrowd 1009 - Salary with Bonus

---
+++ 1009.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    double a, b;
+    char na[100];
+    scanf("%s", na);
+    scanf("%lf", &a);
+    scanf("%lf", &b);
+    printf("TOTAL = R$ %0.2lf\n", (a + (b * 0.15)));
+    return 0;
+}
